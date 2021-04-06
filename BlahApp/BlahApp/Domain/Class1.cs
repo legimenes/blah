20bdca9608828f751695aa20f3a58e118fdfdd76
Leandro <leandru@gmail.com>
2021-04-05 22:09:48 -0300

    initial commit

---
+++ BlahApp/BlahApp/Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace BlahApp.Domain
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ BlahApp/BlahApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using BlahApp.Domain;
+using Newtonsoft.Json;
+using System;
+
+namespace BlahApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string jsonClass1 = GetClass1();
+            Console.WriteLine(jsonClass1);
+        }
+
+        static string GetClass1()
+        {
+            Class1 c1 = new Class1()
+            {
+                Id = 0,
+                Name = "Jon Doe"
+            };
+
+            string json = JsonConvert.SerializeObject(c1);
+
+            return json;
+        }
+    }
+}
